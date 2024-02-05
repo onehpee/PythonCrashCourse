6294e0f76c02f3efc0c5411a9bb2e7c73ab4dcd5
Adedayo UWensuyi <onehpee@yahoo.com>
2024-02-04 22:28:04 -0600

    whitespace and f-strings

--- Variables/Strings.py/full_name.py
+++ Variables/Strings.py/full_name.py
@@ -1,4 +1,11 @@
 first_name = "adedayo"
 last_name = "uwensuyi"
+#f-strings, F stands for format
 full_name = f"{first_name} {last_name}"
 print(full_name)
+print(f"Hello, {full_name.title()}!")
+message = f"Hello, {full_name.title()}!"
+print(message)
+
+#python 3.5 version of format stings
+full_name2 = "{} {}".format(first_name, last_name)
---
+++ Variables/Strings.py/whitespace.py
@@ -0,0 +1,12 @@
+# in programming, whitespace refers to any nonprinting character, such as spaces, tabs, and end-on-line sybols
+# You use white spaces to organize your output and make it more readable
+
+# To add a Tab in a string you use \t
+print("Python")
+print("\tPython")
+
+# To add a New Line in a string you use \n
+print("Language:\nPython\nC\nJavaScript")
+
+# You can also combine them 
+print("Language:\n\tPython\n\tC\n\tJavaScript")
