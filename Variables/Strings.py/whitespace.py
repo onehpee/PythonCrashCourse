be1a08da7543dc2d96d7c277add75ed8780ac2f4
Adedayo UWensuyi <onehpee@yahoo.com>
2024-02-05 21:53:03 -0600

    whitespace and f-strings

--- Variables/Strings.py/whitespace.py
+++ Variables/Strings.py/whitespace.py
@@ -10,3 +10,15 @@ print("Language:\nPython\nC\nJavaScript")
 
 # You can also combine them 
 print("Language:\n\tPython\n\tC\n\tJavaScript")
+
+# Stripping Whitespace
+# Extra whitespace can confuse a program, especially when comparing strings
+
+favorite_language = "'python '"
+print(favorite_language)
+#This is only a temporary change
+print(favorite_language.rstrip())
+# This is the right way to do it, you have to reassign a valuable.
+# This is how a variable's value can be updated as program is executed or in response to user input
+favorite_language = favorite_language.rstrip()
+print(favorite_language)
