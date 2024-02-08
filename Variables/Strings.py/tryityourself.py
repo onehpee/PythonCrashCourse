7ec2acfc597b034164684df740bceff9e6d05cf5
Adedayo UWensuyi <onehpee@yahoo.com>
2024-02-07 22:06:35 -0600

    Exercise

---
+++ Variables/Strings.py/tryityourself.py
@@ -0,0 +1,18 @@
+# 2.3 - 2.7
+
+steve = "Steve"
+
+greetings = f"Hello {steve}, would you like to learn some python today?"
+
+full_name = "steve jobs"
+
+print(full_name.title())
+print(full_name.lower())
+print(full_name.upper())
+
+quote = "Albert Einstein once said, 'A person who never made a mistake never tried anything new'"
+print(quote)
+famous_name = "Albert Einstein"
+quote2 = f"{famous_name} once said, 'A person who never made a mistake never tried anything new'"
+print(quote2)
+
