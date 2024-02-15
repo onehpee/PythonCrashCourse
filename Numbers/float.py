a75fad427a8689f7b20917287604ebf04c3ce7de
Adedayo UWensuyi <onehpee@yahoo.com>
2024-02-14 21:31:40 -0600

    numbers, floats

---
+++ Numbers/float.py
@@ -0,0 +1,12 @@
+first = 0.1 + 0.1
+second = 0.2 + 0.2
+third = 2 * 0.1
+fourth = 2 * 0.2
+fifth = 0.2 + 0.1
+sixth = 2 * 0.2
+print(first)
+print(second)
+print(third)
+print(fourth)
+print(fifth)
+print(sixth)
