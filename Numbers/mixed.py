96f9e30da37e610c6b4d5fb349a32690945bdc3f
Adedayo UWensuyi <onehpee@yahoo.com>
2024-02-14 21:39:52 -0600

    numbers, long numbers, multiple assignments, constants

---
+++ Numbers/mixed.py
@@ -0,0 +1,16 @@
+# To make long numbers more readable you can use underscores to group them together
+universe_age = 14_000_000_000
+
+print(universe_age)
+
+# Multiple Assignments
+x, y, z = 0, 0, 0
+
+print(x)
+print(y)
+print(z)
+
+# Constants
+MAX_CONNECTIONS = 5000
+
+print(MAX_CONNECTIONS)
