e5e38fd0505c5999370bc3bf895828f520281dea
Adedayo UWensuyi <onehpee@yahoo.com>
2024-02-16 23:17:03 -0600

    lists exercise - Your own list

--- Lists/exercise.py
+++ Lists/exercise.py
@@ -17,3 +17,14 @@ print(text_1)
 print(text_2)
 print(text_3)
 print(text_4)
+
+# Your own list
+cars = ['mercedes benz', 'porche', 'ferrari', 'honda']
+statement_1 = f"I would like to own a {cars[0].title()} car one day."
+statement_2 = f"I would like to own a {cars[1].title()} car one day."
+statement_3 = f"I would like to own a {cars[2].title()} car one day."
+statement_4 = f"I would like to own a {cars[3].title()} car one day."
+print(statement_1)
+print(statement_2)
+print(statement_3)
+print(statement_4)
