694ddd52e876a74272e5ea046154f94571be885b
Adedayo UWensuyi <onehpee@yahoo.com>
2024-02-16 23:10:49 -0600

    lists exercise - Greetings

--- Lists/exercise.py
+++ Lists/exercise.py
@@ -7,3 +7,13 @@ print(names[1])
 print(names[2])
 print(names[3])
 
+# Greetings
+# Print a message to each name
+text_1 = f"Hi! My name is {names[0].title()}"
+text_2 = f"Hi! My name is {names[1].title()}"
+text_3 = f"Hi! My name is {names[2].title()}"
+text_4 = f"Hi! My name is {names[3].title()}"
+print(text_1)
+print(text_2)
+print(text_3)
+print(text_4)
