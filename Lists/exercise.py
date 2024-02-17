7e1ed79b0e40eb656a6b7c318b12e38a16a14bf8
Adedayo UWensuyi <onehpee@yahoo.com>
2024-02-16 23:05:55 -0600

    lists exercise - Names

---
+++ Lists/exercise.py
@@ -0,0 +1,9 @@
+# Names
+# Make a list of names
+# Print individual elements of the list
+names = ['Boye', 'Uyi', 'Oseratin', 'Ife']
+print(names[0])
+print(names[1])
+print(names[2])
+print(names[3])
+
