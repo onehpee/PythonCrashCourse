f795ed419ffff076302267ba3a39db2326a7d9da
Adedayo UWensuyi <onehpee@yahoo.com>
2024-02-16 23:00:47 -0600

    lists indexing, individual values from a list

--- Lists/bicycles.py
+++ Lists/bicycles.py
@@ -19,3 +19,7 @@ print(bicycles[3])
 print(bicycles[-1])
 print(bicycles[-2])
 print(bicycles[-3])
+
+# Using individual valuse frm a list
+message = f"My first bicycle was a {bicycles[0].title()}."
+print(message)
