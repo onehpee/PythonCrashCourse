8a5fadb8b701ae7b067c69c9ade1bca71bc4d267
Adedayo UWensuyi <onehpee@yahoo.com>
2024-02-16 22:48:24 -0600

    list.py, elements, manipulation

--- Lists/bicycles.py
+++ Lists/bicycles.py
@@ -1,2 +1,8 @@
 bicycles = ['trek', 'cannondale', 'redline', 'specialized']
 print(bicycles)
+
+# Accessing individual elements in a list
+print(bicycles[0])
+
+# You can also use string methods on any element in this list.
+print(bicycles[0].title())
