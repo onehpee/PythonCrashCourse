ab5517f5a2300a75c6811bb8f25d0d397a4e3fce
Adedayo UWensuyi <onehpee@yahoo.com>
2024-02-22 22:14:50 -0600

    Appending elements to list

--- Lists/motorcycles.py
+++ Lists/motorcycles.py
@@ -1,2 +1,11 @@
 # Modifying Elements in a list
 motorcycles = ['honda', 'yamaha', 'suzuki']
+print(motorcycles)
+
+motorcycles[0] = 'ducati'
+print(motorcycles)
+
+# Adding Elements in a list
+# New elements are added to the end of the list
+motorcycles.append('ducati')
+print(motorcycles)
