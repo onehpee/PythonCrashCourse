ff5c87277205a4a488d841033b21285e520c1177
Adedayo UWensuyi <onehpee@yahoo.com>
2024-02-22 22:21:05 -0600

    Appending elements to list

--- Lists/motorcycles.py
+++ Lists/motorcycles.py
@@ -5,8 +5,8 @@ print(motorcycles)
 motorcycles[0] = 'ducati'
 print(motorcycles)
 
-# Adding Elements in a list
-# New elements are added to the end of the list
+# Apending Elements in a list
+# New elements are added to the end of the list by using the append() method
 motorcycles.append('ducati')
 print(motorcycles)
 
@@ -17,3 +17,10 @@ fruit.append('grape')
 fruit.append('coconut')
 
 print(fruit)
+
+# Inserting into a list
+# YOu can add a new elements at any position in your list by using the insert() method
+
+colors = ['black', 'brown', 'white']
+colors.insert(0, 'yellow')
+print(colors)
