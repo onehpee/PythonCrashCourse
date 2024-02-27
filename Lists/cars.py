168fb0816bc0aef660e60cdbf8d36414343c435d
Adedayo UWensuyi <onehpee@yahoo.com>
2024-02-26 20:16:17 -0600

    Organizing a List - Sort() - cars.py - Reverse

--- Lists/cars.py
+++ Lists/cars.py
@@ -3,3 +3,13 @@
 cars = ['bmw', 'audi', 'toyota', 'subaru']
 cars.sort()
 print(cars)
+
+# The sort method, shown at line 4, changes the order of the list permanently. The cars are
+# now in alphabetical order, and we can never revert to ther original order
+
+cars = ['bmw', 'audi', 'toyota', 'subaru']
+cars.sort(reverse=True)
+print(cars)
+
+# You can also sort this list in reverse alphabetical order by passing the arguement reverse=True
+# to the sort() method. The following example sorts the list of cars in reverse alphabetical order
