13c989015b66fbbf46c80b6088014559ce8220fc
Adedayo UWensuyi <onehpee@yahoo.com>
2024-03-01 22:50:25 -0600

    Looping Through an Entire List - Magicians

--- Lists/magicians.py
+++ Lists/magicians.py
@@ -1,5 +1,5 @@
 # We define a for loop. This line tells Python to pull a name from the lists magicians, and associate it
-# With the magicians
+# With the magicianss
 magicians = ['alice', 'david', 'carolina']
 for magician in magicians:
     print(magician)
