4157596ef5b4314dbcca6a3465cac90ab5fa7694
Adedayo UWensuyi <onehpee@yahoo.com>
2024-03-04 21:11:25 -0600

    Doing more with looping - Magicians

--- Lists/magicians.py
+++ Lists/magicians.py
@@ -2,6 +2,6 @@
 # With the magicianssss
 magicians = ['alice', 'david', 'carolina']
 for magician in magicians:
-    print(magician)
+    print(f"{magician.title()}, that was a great trick!")
     
     
