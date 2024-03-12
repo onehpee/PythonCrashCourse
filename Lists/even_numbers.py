2e1a3f96fec2ae6d1cf5400674e08aecd43c0b2f
Adedayo UWensuyi <onehpee@yahoo.com>
2024-03-11 21:16:05 -0500

    Making numerical List - squares

--- Lists/even_numbers.py
+++ Lists/even_numbers.py
@@ -1,3 +1,6 @@
 # We can also use the range() function to tell Python to skip numbers in a given range.
 # If you pass a third arguement to range(), Python uses that value as a step size when
 # generating numbers
+
+even_numbers = list(range(2, 11, 2))
+print(even_numbers)
