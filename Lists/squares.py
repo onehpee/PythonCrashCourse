080a7fda5e8623bde5c34a33fee45062de1db737
Adedayo UWensuyi <onehpee@yahoo.com>
2024-03-13 21:12:20 -0500

    Making numerical List - Exercise 4.3 - Odd Numbers

--- Lists/squares.py
+++ Lists/squares.py
@@ -27,6 +27,10 @@ for value in range(1, 20):
 one_million = [value for value in range(1, 1000001)]
 print(one_million)
 
+# Summing a Million
 print(min(one_million))
 print(max(one_million))
 print(sum(one_million))
+
+odd_numbers = [value for value in range(1, 20, 2)]
+print(odd_numbers)
