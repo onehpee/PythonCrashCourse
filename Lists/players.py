7b20bd75800e7ba4015f35e174977a29ed3a20ca
Adedayo UWensuyi <onehpee@yahoo.com>
2024-03-15 22:29:10 -0500

    Working with Part of a List - Slicing a List

--- Lists/players.py
+++ Lists/players.py
@@ -1,3 +1,8 @@
 # Slicing a List
 players = ['ade', 'boye', 'princess', 'uyi', 'oso']
 print(players[0:3])
+# The output retains the structure of the list and includes the firt three players in the list
+
+# You can generate any subset of a list. Like second, third, and fourth item in a list
+print(players[1:4])
+
