7d3e898a2bc29ba4050da62145d150678454d550
Adedayo UWensuyi <onehpee@yahoo.com>
2024-03-15 22:40:52 -0500

    Working with Part of a List - Looping Through a Slice

--- Lists/players.py
+++ Lists/players.py
@@ -14,3 +14,11 @@ print(players[2:])
 
 # This prints the names of the last three players and would continue to work as the list of players change  in size
 print(players[-3:])
+
+
+# Looping Through a Slice
+players = ['ade', 'boye', 'princess', 'uyi', 'oso']
+
+print('Here are the first three players on my team: ')
+for player in players[:3]:
+    print(player.title())
