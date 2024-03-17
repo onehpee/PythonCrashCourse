22165b9ac794aa384ab028cc0fdf09d548ee80b0
Adedayo UWensuyi <onehpee@yahoo.com>
2024-03-16 22:21:17 -0500

    Working with Part of a List - Copying a List - foods.py

--- Lists/foods.py
+++ Lists/foods.py
@@ -7,3 +7,6 @@ print(my_foods)
 
 print("\nMy friend's favorite foods are: ")
 print(friend_foods)
+
+# We make a copy of my_foods by asking for a slice of my_foods without specifying any indices
+# And store the copy in friends_foods. When we print each list, we see that they both contain the same foods
