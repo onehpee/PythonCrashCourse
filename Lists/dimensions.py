002b445ec00db1b3a7e60d8490457e71e990dbd7
Adedayo UWensuyi <onehpee@yahoo.com>
2024-03-20 22:32:07 -0500

    Defining a Tuples - dimensions.py

--- Lists/dimensions.py
+++ Lists/dimensions.py
@@ -2,3 +2,9 @@
 dimensions = (200, 50)
 print(dimensions[0])
 print(dimensions[1])
+
+# This won't work and will throw an error because you can't alter a tuple
+# dimensions[0] = 250
+
+# A tuple with one element is typically used but it goes like this
+# my_t = (3,)
