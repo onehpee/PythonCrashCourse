74938787c12feae993407f766579f86a8f09cbc6
Adedayo UWensuyi <onehpee@yahoo.com>
2024-03-20 22:41:18 -0500

    Writing over a Tuple - dimensions.py

--- Lists/dimensions.py
+++ Lists/dimensions.py
@@ -9,6 +9,18 @@ print(dimensions[1])
 # A tuple with one element is typically used but it goes like this
 # my_t = (3,)
 
+
+# Looping over all values in a tuple
 dimensions = (200, 50)
 for dimension in dimensions:
     print(dimension)
+    
+    
+# Writing over a tuple
+# Although you can't modify a tuple, you can assign a new value to a variable that represents a tuple
+# So if we wanted to change our dimensions, we could redefine the entire tuple
+dimensions = (200, 50)
+print("Original dimensions: ")
+for dimension in dimensions:
+    print(dimension)
+    
