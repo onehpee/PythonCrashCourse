269c3de4e9f15c0e9be4f019ed4b4a0047d88a63
Adedayo UWensuyi <onehpee@yahoo.com>
2024-03-20 22:43:02 -0500

    Writing over a Tuple - dimensions.py

--- Lists/dimensions.py
+++ Lists/dimensions.py
@@ -24,3 +24,7 @@ print("Original dimensions: ")
 for dimension in dimensions:
     print(dimension)
     
+dimensions = (400, 100)
+print("\nModified dimensions: ")
+for dimension in dimensions:
+    print(dimension)
