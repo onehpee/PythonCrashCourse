e01e1158cd4b03e288cc733bc21698853ae5978a
Adedayo UWensuyi <onehpee@yahoo.com>
2024-03-25 23:28:05 -0500

    Try it yourself - 5.2 - More Conditional Tests

---
+++ if_statement/conditional_tests.py
@@ -0,0 +1,20 @@
+# Try it Yourself
+# 5.2 More Conditional Tests
+
+string = 'laces'
+
+if string == 'laces':
+    print("Equality")
+    
+if string != 'shoes':
+    print('Inequality')
+    print("Hold the laces!")
+    
+case = 'BANG'
+
+if case == 'BANG':
+    print("True! BANG!")
+    
+if case.lower() == 'BANG':
+    print("False! Dud!")
+    
