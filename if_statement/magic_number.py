07f5db46cfd57fb47e7df32375b3155b6d9d322e
Adedayo UWensuyi <onehpee@yahoo.com>
2024-03-25 23:01:55 -0500

    Try it yourself - 5.1 - Conditional Test

--- if_statement/magic_number.py
+++ if_statement/magic_number.py
@@ -3,3 +3,15 @@ answer = 17
 
 if answer != 42:
     print("That is not the correct answer. Please try again!")
+    
+    
+    
+# Try it yourself
+# Conditional Tests
+
+car = 'subaru'
+print("Is car == 'subaru'? I predict True.")
+print(car == 'subaru')
+
+print("\nIs car == 'audi'? I predict False.")
+print(car == 'audi')
