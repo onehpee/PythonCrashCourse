896285c7dbae05a2facf65f2bddb6a12f85bd479
Adedayo UWensuyi <onehpee@yahoo.com>
2024-04-19 23:11:04 -0500

    Simple if statements - amusement_park.py

--- if_statement/amusement_park.py
+++ if_statement/amusement_park.py
@@ -5,3 +5,12 @@ elif age < 18:
     print("Your admission cost is $25.")
 else:
     print("YOur admission cost is $40.")
+    
+if age < 4:
+    price = 0
+elif age < 18:
+    price = 25
+else:
+    price = 40
+    
+print(f"Your admission cost is ${price}")
