6fb91351d1b7929c32a2d404a324a9f9997eff99
Adedayo UWensuyi <onehpee@yahoo.com>
2024-04-26 22:34:47 -0500

    Checking that a List is Not Empty - toppings.py

--- if_statement/toppingsToo.py
+++ if_statement/toppingsToo.py
@@ -15,3 +15,13 @@ for requested_topping in requested_toppings:
     else:
         print(f"Adding {requested_topping}.")
 print("\nFinished making your pizza!")
+
+
+requested_toppings_toos = []
+
+if requested_toppings_toos:
+    for requested_toppings_too in requested_toppings_toos:
+        print(f"Adding {requested_toppings_too}.")
+    print("\Finished making your pizza!") 
+else:
+    print("Are you sure you want a plain pizza?")
