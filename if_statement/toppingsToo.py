3b83a911529d34d3822da92d209d36b3cfd02c8c
Adedayo UWensuyi <onehpee@yahoo.com>
2024-04-26 22:29:25 -0500

    Checking for Special Items - toppings.py

--- if_statement/toppingsToo.py
+++ if_statement/toppingsToo.py
@@ -8,3 +8,10 @@ if 'pepperoni' in requested_toppings:
 if 'extra cheese' in requested_toppings:
     print("Adding extra cheese.")
 print("\nFinished making your pizza")
+
+for requested_topping in requested_toppings:
+    if requested_topping == 'green peppers':
+        print("Sorry, we are out of green peppers right now.")
+    else:
+        print(f"Adding {requested_topping}.")
+print("\nFinished making your pizza!")
