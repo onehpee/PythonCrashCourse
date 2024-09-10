5446d04b471f2d73be45dbed3826f716a8477712
Adedayo UWensuyi <onehpee@yahoo.com>
2024-09-09 22:20:36 -0500

    project

--- Dictionary/alien.py
+++ Dictionary/alien.py
@@ -5,5 +5,11 @@ print(alien_0['points'])
 
 alien_0 = {'color': 'green'}
 
+
+alien_0 = {'color': 'green', 'points': 5}
 new_points = alien_0['points']
 print(f"You just earned {new_points} points!")
+
+alien_0 = {'color': 'green', 'points': 5}
+print(alien_0)
+
